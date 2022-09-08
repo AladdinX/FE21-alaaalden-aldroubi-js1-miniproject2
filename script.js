aee0a604c42aff7d8fe04000f91d7da32e85c3f0
Alaa alden Aldroubi <Alaa.aldroubi@gmail.com>
2022-09-08 23:03:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
+const body = document.querySelector('body');
+const stenbtn = document.querySelector('#sten');
+const saxbtn = document.querySelector('#sax');
+const papperbtn = document.querySelector('#papper');
+
+
 let input = document.querySelector('input');
 let userName = document.querySelector('#userName');
-const body = document.querySelector('body');
+let userVal = document.querySelector('#userVal');
+let usrPng = document.querySelector('#userPng');
+let datorVal = Math.round(Math.random() * 3);
+let pcVal = document.querySelector('#pcVal');
+let pcPng = document.querySelector('#pcPng');
+let poanguser = 0;
+let poangpc = 0;
+
 input.addEventListener('keyup', function (event) {
   userName.innerText = `Hello ${input.value} lets start playing ! ` + "\n\n\n\n" + 'choose:';
   if (event.key === 'Enter') {
     input.style.display = 'none';
   }
 })
-
-let datorVal = Math.round(Math.random() * 3);
-const stenbtn = document.querySelector('#sten');
-const saxbtn = document.querySelector('#sax');
-const papperbtn = document.querySelector('#papper');
-let userVal = document.querySelector('#userVal');
-let pcVal = document.querySelector('#pcVal');
-let usrPng = document.querySelector('#userPng');
-let pcPng = document.querySelector('#pcPng');
-let poanguser = 0;
-let poangpc = 0;
 
 stenbtn.addEventListener('click', function () {
   let datorVal = Math.round(Math.random() * 3);
